f47c37ab099013291ce38fbf762340fd968af1cc
TTTriplicate <tttriplicate@gmail.com>
2024-05-16 21:03:55 -0400

    getting even more set to go

--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,13 +1,23 @@
 import * as cdk from "aws-cdk-lib";
 import * as dynamo from 'aws-cdk-lib/aws-dynamodb';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apg from 'aws-cdk-lib/aws-apigatewayv2'
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const gateway = new apg.HttpApi(this, 'intakeApi', {
+      description: 'Intake API for weather data'
+    })
+
+    const intakeLambda = new lambda.Function(this, 'intakeLambda', {
+      runtime: lambda.Runtime.FROM_IMAGE,
+      code: lambda.Code.fromDockerBuild(``),
+      handler: 'hello'
+    })
     const storage = new dynamo.TableV2(this, "WeatherTrack", {
       partitionKey: { name: "PK", type: dynamo.AttributeType.NUMBER },
       sortKey: {
